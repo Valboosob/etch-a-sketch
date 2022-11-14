d825dccde0017d48fe707be45829b9b18d55e564
Valboosob <klaudia.karate@gmail.com>
2022-11-14 14:26:16 +0100

    Made element deleting squares when setting grid

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-const box = document.getElementById('box');
 
-for (let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('class', 'square');
+const button = document.querySelector('.button');
+let width = "0";
+let height = "0";
+let clickCount = "0";
 
-    box.append(div);
+button.addEventListener('click', function buttonClicked() {
+        width = prompt("Width:", " ");
+        height = prompt("Height:", " ");
+        ++clickCount;
+        gridSize();
+});
+
+
+function gridSize() {
+    let allSquares = width * height;
+    box.style.width = `${width * 39}` + "px"; 
+    box.style.height = `${height * 36.8}` + "px";
+
+    if ( clickCount == 1) {
+        for (let i = 0; i < allSquares; i++) {
+            const box = document.getElementById('box')
+            const div = document.createElement('div');
+            div.setAttribute('class', 'square');
+            box.appendChild(div);
+        }
+    } else if ( clickCount > 1) {
+        const box = document.getElementById('box');
+        box.innerHTML = "";
+
+        for (let i = 0; i < allSquares; i++) {
+            const box = document.getElementById('box')
+            const div = document.createElement('div');
+            div.setAttribute('class', 'square');
+            box.appendChild(div);
+        }  
+    }
+
 }
-
